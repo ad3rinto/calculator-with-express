0f8747147023b46adea8bf87619773d7eb21a957
ad3rinto <aad3rinto@gmail.com>
2023-04-09 19:54:14 +0100

    amended port due to error

--- calculator.js
+++ calculator.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("<h1>Hello, world</h1>")
+    res.send("<h1>Hello, calculator world</h1>")
 })
 
 
-app.listen(3000, () => {
-    console.log("listening on port 3000")
+app.listen(3001, () => {
+    console.log("listening on port 3001")
 })
