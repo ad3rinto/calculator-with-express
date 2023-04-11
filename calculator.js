c9b972c7d9be2ddfb8ea3725ba644ab833c112f1
ad3rinto <aad3rinto@gmail.com>
2023-04-11 18:52:05 +0100

    built simple BMI calculator

--- calculator.js
+++ calculator.js
@@ -11,10 +11,25 @@ app.get("/", function (req, res) {
 
 
 app.post("/", function (req, res) {
-    console.log(req.body)
-    res.send("<h1>Thanks for posting</h1>")
+    const num1 = Number(req.body.num1)
+    const num2 = Number(req.body.num2)
+    res.send(`<h1>Adding these two numbers gives ${num1 + num2}</h1>`)
 
 })
+
+app.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", function (req, res) {
+    const weight = Number(req.body.weight)
+    const height = Number(req.body.height)
+    const bmi = Math.round(weight / height ** 2)
+    res.send(`<h1>Your BMI calculates as ${bmi}</h1>`)
+
+})
+
+
 
 app.listen(3001, () => {
     console.log("listening on port 3001")
